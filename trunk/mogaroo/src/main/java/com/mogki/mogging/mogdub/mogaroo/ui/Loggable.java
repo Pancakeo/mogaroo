004ce08e98f71d888314ad465c448fa16b8ac280
JesseTheGauntt@gmail.com <JesseTheGauntt@gmail.com@2fcd676f-1dc5-15e5-cbf2-54177c6d0eee>
2011-11-22 07:59:08 +0000

    Removing mogaroo folder

--- trunk/mogaroo/src/main/java/com/mogki/mogging/mogdub/mogaroo/ui/Loggable.java
+++
@@ -1,9 +0,0 @@
-package com.mogki.mogging.mogdub.mogaroo.ui;
-
-public interface Loggable {
-	/** Append a message. **/
-	public void appendMessage(String message);
-	
-	/** Clears this log. **/
-	//public void clearLog();
-}
