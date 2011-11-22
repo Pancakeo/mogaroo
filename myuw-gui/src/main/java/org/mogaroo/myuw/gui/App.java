0f168e36b86517110e7198b4790d456970eb61d9
JesseTheGauntt@gmail.com <JesseTheGauntt@gmail.com@2fcd676f-1dc5-15e5-cbf2-54177c6d0eee>
2011-11-22 08:05:58 +0000

    Adding gui folder

---
+++ myuw-gui/src/main/java/org/mogaroo/myuw/gui/App.java
@@ -0,0 +1,13 @@
+package org.mogaroo.myuw.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
